c5a5b808ee6cfce47d379c390ae6d3b8d29c67ec
Lute92 <sithukyaw992@gmail.com>
2017-05-05 13:27:52 +0630

    Second Commit

--- src/api.js
+++ src/api.js
@@ -9,41 +9,17 @@ Api.prototype = function(){
 
      $.ajax({
       type: 'GET',
-      url :'/data.json',
+      url :'src/data.json',
       success: function(result){
-        callback(data);
-      };
+        callback(result);
+      },
       error: function(error){
         console.log(error);
-      };
+      }
     });
 
   };
-
-  let getStudentById = function(studentId, callback){
-
-    $.ajax({
-
-     type: 'GET',
-     url :'/data.json',
-     success: function(result){
-
-       $.each(result, function(index,student){
-         if(student.id === studentId){
-           callback(student);
-           return false;
-         }
-       });
-     };
-     error: function(error){
-       console.log(error);
-     };
-    });
-  };
-
-
   return{
-    getStudentById : getStudentById,
     getStudents : getStudents
   }
 }();
--- src/studentApp.js
+++ src/studentApp.js
@@ -4,22 +4,22 @@ let StudentApp = function(){
 };
 
 StudentApp.prototype = function(){
-  let api = new Api();
-
   let loadStudents = function(){
+    let api = new Api();
+
     $.when(
       loadTemplate("templates/student-row.html")
     ).done(function(template){
 
       $('#student-list').empty();
-      $('#student-list').append(template);
-
       api.getStudents(function(students){
 
         $.each(students, function(index, student){
 
+          $('#student-list').append(template);//Add student row
           let currentRowView = $(document).find("div[name=student-row]").eq(index);
-          $(currentRowView).find("label[name=lblId]").html(student.id);
+
+          $(currentRowView).find("label[name=lblId]").html(student.id);//Assign data to related elements
           $(currentRowView).find("label[name=lblName]").html(student.name);
           $(currentRowView).find("label[name=lblAge]").html(student.age);
           $(currentRowView).find("label[name=lblAddress]").html(student.address);
